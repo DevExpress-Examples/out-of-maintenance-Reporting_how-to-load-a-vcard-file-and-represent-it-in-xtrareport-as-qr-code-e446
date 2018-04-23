f1f74024bfd587564a96438ec9f989b64bf87362
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to load a VCard file and represent it in XtraReport as QR code
+
+
+<p>This example illustrates how to load *.vcf files and render them in XtraReport in QR code.</p>
+
+<br/>
+
+
---
+++ VB/WindowsFormsApplication1/XtraReport1.Designer.vb
@@ -0,0 +1,103 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace WindowsFormsApplication1
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim qrCodeGenerator1 As New DevExpress.XtraPrinting.BarCode.QRCodeGenerator()
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+			Me.xrBarCode1 = New DevExpress.XtraReports.UI.XRBarCode()
+			Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+			CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel1, Me.xrBarCode1})
+			Me.Detail.HeightF = 344.7917F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' TopMargin
+			' 
+			Me.TopMargin.Name = "TopMargin"
+			Me.TopMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' BottomMargin
+			' 
+			Me.BottomMargin.Name = "BottomMargin"
+			Me.BottomMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrBarCode1
+			' 
+			Me.xrBarCode1.AutoModule = True
+			Me.xrBarCode1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("BinaryData", Nothing, "Vcard")})
+			Me.xrBarCode1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 0F)
+			Me.xrBarCode1.Name = "xrBarCode1"
+			Me.xrBarCode1.Padding = New DevExpress.XtraPrinting.PaddingInfo(10, 10, 0, 0, 100F)
+			Me.xrBarCode1.SizeF = New System.Drawing.SizeF(367.7083F, 344.7917F)
+			qrCodeGenerator1.CompactionMode = DevExpress.XtraPrinting.BarCode.QRCodeCompactionMode.Byte
+			qrCodeGenerator1.Version = DevExpress.XtraPrinting.BarCode.QRCodeVersion.Version1
+			Me.xrBarCode1.Symbology = qrCodeGenerator1
+			' 
+			' xrLabel1
+			' 
+			Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Path")})
+			Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(367.7084F, 0F)
+			Me.xrLabel1.Name = "xrLabel1"
+			Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel1.SizeF = New System.Drawing.SizeF(282.2916F, 344.7917F)
+			Me.xrLabel1.Text = "xrLabel1"
+			' 
+			' bindingSource1
+			' 
+			Me.bindingSource1.DataSource = GetType(WindowsFormsApplication1.MyObj)
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.TopMargin, Me.BottomMargin})
+			Me.DataSource = Me.bindingSource1
+			Me.Version = "12.2"
+			CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+		Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+		Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+		Private xrBarCode1 As DevExpress.XtraReports.UI.XRBarCode
+		Private bindingSource1 As System.Windows.Forms.BindingSource
+	End Class
+End Namespace
